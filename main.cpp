8e84f6b3c5baaed731a7450ed354dd53c3c1e578
PCPAPC172 <apkme11e@gmail.com>
2025-12-08 19:43:55 +0330

    fix linux font

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include "MainWindow.h"
 #include <QApplication>
-#include <QFontDatabase>
+#include <QFont>
+#include <QPalette>
+#include <QColor>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
-    
-    // Set a nice default font
-    QFont font("Segoe UI", 10);
+
+    // Set a cross-platform default font
+    QFont font("Sans Serif", 10);
+    font.setStyleStrategy(QFont::PreferAntialias);
     a.setFont(font);
-    
+
     // Telegram-like Palette
     QPalette p = a.palette();
     p.setColor(QPalette::Window, QColor(245, 245, 245));
